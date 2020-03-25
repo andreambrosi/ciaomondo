1b4226646f93ab206e57af985ee3854e28953fe3
Andrea@LAPTOP-VJTD03FH <Andrea@LAPTOP-VJTD03TH>
2020-03-25 17:01:16 +0100

    Start commit

---
+++ contains.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Andrea on 19/03/2020.
+//
+#include "contains.h"
+
+bool isContain(int a1[], int a2[], int size1, int size2) {
+    for (int i = 0; i < (size1 - size2); ++i) {
+        if(a1[i] == a2[0]) {
+            bool flag = true;
+            for(int k = 1; k < size2; k++) {
+                if(a1[i + k] != a2[k]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag)
+                return true;
+        }
+    }
+
+    return false;
+}
